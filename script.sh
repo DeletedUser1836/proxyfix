6d6434540a0146a9cbb9bfa1ffb49424d103be8d
DeletedUser1836 <den.pylypen@gmail.com>
2025-05-04 11:07:25 +0200

    reading of first arguments for command

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+# If we want to change params in proxychains config file
+if [ $1 -eq "-c" ]
+then
+    sudo nano /etc/proxychains4.conf
+fi
+
+if [ $1 -eq "-CPS" ]
+then
+   sudo nano /etc/proxychains4.conf && 
