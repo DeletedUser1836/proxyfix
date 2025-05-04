7ace21f0f1ca57a0b46c7c8824c8ac41b4401ee4
DeletedUser1836 <den.pylypen@gmail.com>
2025-05-04 12:23:18 +0200

    an catual first version cause i dunno how to commit things xd

--- script.sh
+++ script.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
-# If we want to change params in proxychains config file
-if [ $1 -eq "-c" ]
-then
-    sudo nano /etc/proxychains4.conf
-fi
+# Find proxychains config file
+PROXCONF=$(locate proxychains | grep -E '\.conf$' | head -n 1)
 
-if [ $1 -eq "-CPS" ]
-then
-   sudo nano /etc/proxychains4.conf && 
+echo "Config file detected: $PROXCONF"
+
+#edit the whole file with nano
+if [[ "$1" == "-E" || "--edit"]]; then
+    echo "Opening proxychains config file: $PROXCONF"
+    sudo nano "$PROXCONF"
+
+#list proxies
+elif [[ "$1" == "--list" || "$1" == "-l" ]]; then
+    echo "Listing proxies..."; echo " "
+    echo "type | ip | port|"
+    grep -E '^socks|^http|^https' "$PROXCONF"
+
+#edit the list of proxies
+elif [[ "$1" == "--edit-list" || "$1" == "-el" ]]; then
+    echo "Editing active proxies list..."
+    sudo nano "$PROXCONF"
+
+#help page
+elif [[ "$1" == "-h" || "$1" == "-H" || "$1" == "-?" || "$1" == "--help" ]]; then
+    echo "> proxfix arguments:"
+    echo "> proxfix -E OR --edit                # Edit whole proxychains config file"
+    echo "> proxfix --list OR -l                # List active proxies"
+    echo "> proxfix --edit-list OR -el          # Edit proxy list only"
+ 
+#invalid command 
+else
+    echo "Invalid argument. Use -h for help."
+fi
