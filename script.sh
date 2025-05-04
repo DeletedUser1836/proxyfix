0fc2b25fde67e8eb5f03a36da9e7197a76945e1c
DeletedUser1836 <den.pylypen@gmail.com>
2025-05-04 15:39:52 +0200

    clear function + installing with user input if proxychains is not installed
    i am a silly billy

--- script.sh
+++ script.sh
@@ -19,6 +19,28 @@ then
         if [[ $(which proxychains) != /usr/bin/proxychains ]]
         then
             echo "Error: proxychains is not installed"
+            echo "Do you want to install it?[y/n]"
+            while true
+            do
+                read confirm0
+                case "$confirm0" in
+                    y)
+                        sudo apt install proxychains
+                        echo ""
+                        echo "You've installed proxychains. Run 'proxyfix -h' for help" 
+                        exit 0
+                    ;;
+
+                    n)
+                        echo "Abort."
+                        break
+                    ;;
+
+                    *)
+                        echo -n "Incorrect answer, please answer again with 'y' or 'n': "
+                    ;;
+                esac
+            done
         fi
         exit 1
     fi
@@ -38,10 +60,21 @@ case $1 in
         grep -E '^\s*(socks4|socks5|http|https)\s+' "$PROXCONF" | awk '{ printf "%-8s | %-12s | %s\n", $1, $2, $3 }'
     ;;
 
+    -ela|--edit-list-add)
+        ADD_MODE=true
+        shift
+        set -- -el "$@"
+        ;&
+    -elcl|--edit-list-clear)
+        CLEAR_MODE=true
+        shift
+        set -- -el "$@"
+        ;&
     -el|--edit-list)
         shift
 
-        ADD_MODE=false
+        ADD_MODE=${ADD_MODE:-false}
+        CLEAR_MODE=${CLEAR_MODE:-false}
         PROXY_LINES=()
 
         while [[ "$1" =~ ^- ]]
@@ -97,20 +130,58 @@ case $1 in
             grep -vE '^\s*(socks4|socks5|http|https)\s+' "$PROXCONF" > "$TMP_CONF"
 
             cat "$TMP_CONF" > "$PROXCONF"
+
+            if [[ "$CLEAR_MODE" == true ]]
+            then
+                echo "# Cleared proxy list" >> "$PROXCONF"
+            fi
+
             for line in "${PROXY_LINES[@]}"
             do
                 echo "$line" >> "$PROXCONF"
             done
 
             echo "Proxy list updated in $PROXCONF"
+            rm "$TMP_CONF"
         fi
+    ;;
+
+    -cl|--clear)
+        echo "Are you sure that you want to clear the proxy list? [y/n]"
+        while true
+        do
+            read confirm2
+            case "$confirm2" in
+                y)
+                    echo "Clearing proxy list in $PROXCONF..."
+                    TMP_CONF=$(mktemp)
+                    grep -vE '^\s*(socks4|socks5|http|https)\s+' "$PROXCONF" > "$TMP_CONF"
+                    cat "$TMP_CONF" > "$PROXCONF"
+                    rm "$TMP_CONF"
+                    echo "Proxy list cleared."
+                    break
+                ;;
+
+                n)
+                    echo "Abort."
+                    break
+                ;;
+
+                *)
+                    echo -n "Incorrect answer, please answer again with 'y' or 'n': "
+                ;;
+            esac
+        done
     ;;
 
     -h|-H|-?|--help)
         echo "> $(basename "$0") arguments:"
         echo "> -E OR --edit                # Edit entire proxychains config file"
-        echo "> --list OR -l                # List active proxies"
-        echo "> --edit-list OR -el          # Edit proxy list only"
+        echo "> -l OR --list                # List active proxies"
+        echo "> -el OR --edit-list          # Replace proxy list"
+        echo "> -ela OR --edit-list-add     # Add to current proxy list"
+        echo "> -elcl OR --edit-list-clear  # Clear proxy list before editing"
+        echo "> -cl OR --clear              # Just clear proxy list"
         echo "> -h OR any help flag         # Show this help message"
     ;;
 
